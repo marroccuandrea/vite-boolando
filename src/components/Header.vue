70cf7b890f6a2ce73a5ee0fe7db9d78845eadc55
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-21 16:47:17 +0100

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <div>
+            <ul>
+                <li><a href="#">Donna</a></li>
+                <li><a href="#">Uomo</a></li>
+                <li><a href="#">Bambino</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+@use '../assets/scss/partials/variables' as *;
+
+header {
+    height: 50px;
+    background-color: $orange-h;
+}
+</style>
