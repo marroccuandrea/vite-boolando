92ab594807fc83e647783d6d50348e0d18e05dcd
marroccuandrea <marroccuandrea@yahoo.it>
2024-03-21 17:26:19 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,22 +6,57 @@ export default {
 
 <template>
     <header>
-        <div>
-            <ul>
-                <li><a href="#">Donna</a></li>
-                <li><a href="#">Uomo</a></li>
-                <li><a href="#">Bambino</a></li>
-            </ul>
+        <div class="banner">
+            <div>
+                <ul>
+                    <li><a href="#">Donna</a></li>
+                    <li><a href="#">Uomo</a></li>
+                    <li><a href="#">Bambino</a></li>
+                </ul>
+            </div>
+            <img class="logo" src="../assets/scss/img/boolean-logo.png" alt="">
+            <div>
+                <ul>
+                    <li><i class="fa-regular fa-user"></i></li>
+                    <li><i class="fa-regular fa-heart"></i></li>
+                    <li><i class="fa-solid fa-bag-shopping"></i></li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
-@use '../assets/scss/partials/variables' as *;
 
 header {
-    height: 50px;
+    display: flex;
+    align-items: center;
+    height: 70px;
     background-color: $orange-h;
+    color: $bianco;
+
+}
+
+.banner {
+    max-width: 1200px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+
+    li {
+        padding: 15px;
+
+        a {
+            text-decoration: none;
+            color: $bianco;
+        }
+    }
 }
 </style>
